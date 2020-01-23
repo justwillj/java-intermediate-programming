ff397e2456c2c3c30425c0a45531e4ad16828eb1
Greg Fisher <gfisher@catalyte.io>
2020-01-23 09:32:30 -0800

    Initial Commit

---
+++ src/main/java/io/catalyte/training/LogicProblems.java
@@ -0,0 +1,15 @@
+package io.catalyte.training;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface LogicProblems {
+
+  public Double average(int[] scores);
+
+  public int lastWordLength(String text);
+
+  public BigDecimal distinctLadderPaths(int rungs);
+  
+  public List<List<String>> groupStrings(String[] strs);
+}
---
+++ src/test/java/io/catalyte/training/LogicProblemsImplTest.java
@@ -0,0 +1,7 @@
+package io.catalyte.training;
+
+import static org.junit.Assert.*;
+
+public class LogicProblemsImplTest {
+  //TODO: Implement all requirements as specified in the requirements document
+}
