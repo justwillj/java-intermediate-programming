ee4290ab2d86862719e4acd40dbeb14e807039e3
Greg Fisher <gfisher@catalyte.io>
2020-03-27 08:47:51 -0700

    Add instructions to NOT change the interface

--- src/main/java/io/catalyte/training/LogicProblems.java
+++ src/main/java/io/catalyte/training/LogicProblems.java
@@ -3,13 +3,26 @@ package io.catalyte.training;
 import java.math.BigDecimal;
 import java.util.List;
 
+/*
+ ***********************************************************
+ ***********************************************************
+ ***********************************************************
+ **************DO NOT CHANGE THIS INTERFACE!!!**************
+ ***********************************************************
+ ***********************************************************
+ ***********************************************************
+ */
 public interface LogicProblems {
 
+  //DO NOT CHANGE THIS METHOD SIGNATURE
   public Double average(int[] scores);
 
+  //DO NOT CHANGE THIS METHOD SIGNATURE
   public int lastWordLength(String text);
 
+  //DO NOT CHANGE THIS METHOD SIGNATURE
   public BigDecimal distinctLadderPaths(int rungs);
 
+  //DO NOT CHANGE THIS METHOD SIGNATURE
   public List<List<String>> groupStrings(String[] strs);
 }
