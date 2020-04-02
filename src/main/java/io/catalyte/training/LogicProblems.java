ff5a207db79ecefa3f3d4561bf23bb4db13f4830
Kyle Pike <kpike@catalystsolves.com>
2020-04-02 11:13:20 -0400

    Amended comments to allow for JavaDoc

--- src/main/java/io/catalyte/training/LogicProblems.java
+++ src/main/java/io/catalyte/training/LogicProblems.java
@@ -9,20 +9,21 @@ import java.util.List;
  ***********************************************************
  **************DO NOT CHANGE THIS INTERFACE!!!**************
  ***********************************************************
+ ******** EXCEPTION: TO ADD JAVADOC STYLE COMMENTS *********
+ ******************TO THE PUBLIC METHODS********************
  ***********************************************************
  ***********************************************************
  */
-public interface LogicProblems {
 
-  //DO NOT CHANGE THIS METHOD SIGNATURE
+
+public interface LogicProblems {
+  //DO NOT CHANGE THE METHOD SIGNATURES BELOW
+
   public Double average(int[] scores);
 
-  //DO NOT CHANGE THIS METHOD SIGNATURE
   public int lastWordLength(String text);
 
-  //DO NOT CHANGE THIS METHOD SIGNATURE
   public BigDecimal distinctLadderPaths(int rungs);
 
-  //DO NOT CHANGE THIS METHOD SIGNATURE
   public List<List<String>> groupStrings(String[] strs);
 }
