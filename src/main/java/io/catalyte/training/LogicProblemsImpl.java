045371169c435fec3daece436b82763b8d094c55
Justin Williams <williams.jman@gmail.com>
2022-12-04 22:13:35 -0500

    Created a method to return the average of a int array

--- src/main/java/io/catalyte/training/LogicProblemsImpl.java
+++ src/main/java/io/catalyte/training/LogicProblemsImpl.java
@@ -1,11 +1,27 @@
 package io.catalyte.training;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class LogicProblemsImpl implements LogicProblems {
     @Override
     public Double average(int[] scores) {
-        return null;
+        double total = 0.00;
+        double avg = 0.00;
+
+       if (scores.length ==0){
+          return  0.00;
+      }
+        for (int score : scores) {
+            total = total + score;
+        }
+        avg = total / scores.length;
+
+        DecimalFormat avgTwo = new DecimalFormat("#0.00");
+        avg= Double.parseDouble(avgTwo.format(avg));
+        System.out.println(avg);
+        return avg;
     }
 
     @Override
--- src/test/java/io/catalyte/training/LogicProblemsImplTest.java
+++ src/test/java/io/catalyte/training/LogicProblemsImplTest.java
@@ -1,5 +1,16 @@
 package io.catalyte.training;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LogicProblemsImplTest {
-  //TODO: Implement all requirements as specified in the requirements document
+    LogicProblemsImpl exercise = new LogicProblemsImpl();
+    @Test
+    void averageGoodTest() {
+        int[] test = {1,2,4};
+        Double expected= 2.33;
+        Double result = exercise.average(test);
+        assertEquals(expected,result, ()-> "Wrong answer was given" + result);
+    }
+    //TODO: Implement all requirements as specified in the requirements document
 }
